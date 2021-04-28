878d8eb3081f4148141b3b0c9d77f1b18e2fd692
unknown <17803658+Brian-Dawson@users.noreply.github.com>
2021-04-28 16:20:13 -0400

    more done idk

--- lab4.srcs/sim_1/new/testbench.sv
+++ lab4.srcs/sim_1/new/testbench.sv
@@ -11,13 +11,14 @@ module testbench(
     reg [6:0] KN;
     reg [6:0] K;
 reg [6:0] N;
-reg [6:0] file [0:6];
 reg trace1;
 reg trace2;
 wire [6:0] pass_1;
 wire [6:0] pass_2;
 reg [6:0] HIT;
 reg [6:0] MISS;
+reg [23:0] array_inputs1 [57961:0];
+reg [23:0] array_inputs2 [58596:0];
     
     cache_main cache_main(
     .clk(clk),
@@ -25,7 +26,8 @@ reg [6:0] MISS;
     .KN(KN),
     .K(K),
     .N(N),
-    .file(file),
+    .file1(array_inputs1),
+    .file2(array_inputs2),
     .trace1(trace1),
     .trace2(trace2),
     .pass_1(pass_1),
@@ -35,6 +37,10 @@ reg [6:0] MISS;
     initial begin
         clk = 0;
         reset = 1;
+        KN = 256;
+        K = 2;
+        N = 128;
+        trace1 = 1;
     end
     
     always @(*)
@@ -46,25 +52,31 @@ reg [6:0] MISS;
 
     
    integer file_inputs2;
-   integer scan_inputs2_num;
+   //integer scan_inputs2_num;
    integer file_inputs1;
-   integer scan_inputs1_num;
+  // integer scan_inputs1_num;
    
-   
-   reg [23:0] captured_inputs2 [0:59999];
-   reg [23:0] captured_inputs1 [59999:0];
+   reg [23:0] captured_inputs1;
+   reg [23:0] captured_inputs2;
+  // reg [23:0] captured_inputs2 [0:59999];
+  // reg [23:0] captured_inputs1 [59999:0];
    
    initial begin
    
-        file_inputs1 = $fopen("D:/Verilogprojects/ELC463/lab4.srcs/sim_1/new/TRACE1.DAT", "r");
+       // file_inputs1 = $fopen("D:/Verilogprojects/ELC463/lab4.srcs/sim_1/new/TRACE1.TXT", "r");
+            file_inputs1 = $fopen("D:/Verilogprojects/ELC463/lab4.srcs/sim_1/new/TRACE1.DAT", "rb");
         
         if(file_inputs1 == 0)
             begin
                 $display("file not found");
                 $finish;
             end
+end
+
+    initial begin
             
-        file_inputs2 = $fopen("D:/Verilogprojects/ELC463/lab4.srcs/sim_1/new/TRACE2.DAT", "r");
+        //file_inputs2 = $fopen("D:/Verilogprojects/ELC463/lab4.srcs/sim_1/new/trace2.txt", "r");
+        file_inputs2 = $fopen("D:/Verilogprojects/ELC463/lab4.srcs/sim_1/new/TRACE2.DAT", "rb");
         
         if(file_inputs2 == 0)
             begin
@@ -73,22 +85,35 @@ reg [6:0] MISS;
             end
    end
    
-   
+   integer r;
+   integer y;
     initial begin
+        r = 0;
         while(!$feof(file_inputs1))
             begin
-                //scan_inputs1 = ($fgetc(file_inputs1)) | ($fgetc(file_inputs1) << 8) | ($fgetc(file_inputs1) << 16);
-                scan_inputs1_num =  $fgets(captured_inputs1, file_inputs1);
+                captured_inputs1 = ($fgetc(file_inputs1)) | ($fgetc(file_inputs1) << 8) | ($fgetc(file_inputs1) << 16);
+                //scan_inputs1_num =  $fgets(captured_inputs1, file_inputs1);
+                //$display("captured input: %d file_input: %d", captured_inputs1, file_inputs1);
+                r = r + 1;
+                array_inputs1[r] = captured_inputs1;
+                //$display("Input: 0x%0h", captured_inputs1);
             end  
+            //$display("Number of references: 0x%0h", r);
                 $fclose(file_inputs1);
     end
     
     initial begin
+    y = 0;
         while(!$feof(file_inputs2))
             begin
-                 //scan_inputs2 = ($fgetc(file_inputs2)) | ($fgetc(file_inputs2) << 8) | ($fgetc(file_inputs2) << 16);
-                 scan_inputs2_num = $fgets(captured_inputs2, file_inputs2);
+                 captured_inputs2 = ($fgetc(file_inputs2)) | ($fgetc(file_inputs2) << 8) | ($fgetc(file_inputs2) << 16);
+                 //scan_inputs2_num = $fgets(captured_inputs2, file_inputs2);
+                 //$display("captured input: %d file_input: %d", captured_inputs2, file_inputs2);
+                 y = y + 1;
+                 array_inputs2[y] = captured_inputs2;
+                 //$display("0x%0h", captured_inputs2);
             end
+            //$display("0x%0h", y);
                 $fclose(file_inputs2);
     end
     
--- lab4.srcs/sources_1/new/cache_sim.v
+++ lab4.srcs/sources_1/new/cache_sim.v
@@ -7,7 +7,27 @@
 function set_counter(
 input index_loc,
 input [6:0] N,
-input [6:0] cache [6:0]
+input [23:0] cache [57961:0]
+);
+
+integer inter_set_cntr;
+inter_set_cntr = cache[index_loc][N+1];
+
+    if(inter_set_cntr == N)
+        begin
+            inter_set_cntr = 1;
+        end
+    else
+        begin
+            inter_set_cntr += 1;
+        end
+
+endfunction
+
+function set_counter_2(
+input index_loc,
+input [6:0] N,
+input [23:0] cache [58596:0]
 );
 
 integer inter_set_cntr;
@@ -30,14 +50,18 @@ input reset,
 input [6:0] KN,
 input [6:0] K,
 input [6:0] N,
-input [6:0] file [0:6],
+input [23:0] file1 [57961:0],
+input [23:0] file2 [58596:0],
 input trace1,
 input trace2,
 output reg [6:0] pass_1,
 output reg [6:0] pass_2
     );
-    reg [6:0] data [0:6];
-    reg [6:0] reordered_data [0:6];
+    reg [23:0] data_1 [57961:0];
+    reg [23:0] data_2 [58596:0];
+    reg [23:0] reordered_data_1 [57961:0];
+    reg [23:0] reordered_data_2 [58596:0];
+    reg [2:0] bit_part;
     integer hit;
     integer miss;
     integer i;
@@ -54,52 +78,69 @@ output reg [6:0] pass_2
     integer internal_1 = 0; //MISS
     integer internal_2 = 0; //HIT
     
-    
-    
-    
+    always@(*)begin
+    for(int q = 0; q < 57961; q = q + 1)
+    begin
+    data_1[q] = file1[q];
+    end
+    for(int o = 0; o < 58596; o = o + 1)
+    begin
+    data_2[o] = file2[o];
+    end
+    end
+ 
 
     always @(trace1) begin
     for(a = 0; a < 57961; a = a + 1)
     begin
-    reordered_data [a] = data [a + 2];
-    reordered_data [a + 1] = data [a + 1];
-    reordered_data [a + 2] = data [a];
+   // reordered_data_1[a][23:0] = data_1[a][23:0];
+   // reordered_data_1[a][23:0] = data_1[temp][23:0];
+   // reordered_data_1[a][23:0] = data_1[a][23:0];
+   
+    bit_part[0] = (data_1[a] && 'h0000FF) << 16;
+    bit_part[1] = ((data_1[a] && 'h00FF00) >> 8) << 8;
+    bit_part[2] = (data_1[a] && 'hFF0000) >> 16;
+    
+    reordered_data_1[a][23:16] = bit_part[2];
+    reordered_data_1[a][15:8] = bit_part[1];
+    reordered_data_1[a][7:0] = bit_part[0];
+    
     end
     
     for(i = 0; i < 57961; i = i + 1)
         begin
             for(h = 0; h < K; h = h + 1)
                 begin
-                    if(data[h][0] == i[21:20])
+                    if(reordered_data_1[h][0] == i[21:20])
                         begin
                             k_inner = 1;
                             hit_flag = 0;
                             for(j = 0; j < N+1; j = j + 1)
                                 begin
-                                    if(data[h][k] == entry[20:0])
+                                    if(reordered_data_1[h][k] == entry[20:0])
                                         begin
                                             hit_flag = 1;
                                         end
                                 end
-                            if(data[h][set_cntr] == 0)
+                            if(reordered_data_1[h][set_cntr] == 0)
                                 begin
-                                    data[h][set_cntr] = entry[20:0];
+                                    reordered_data_1[h][set_cntr] = entry[20:0];
                                     internal_1 += 1;
-                                    set_cntr = set_counter(h, N, data);
-                                    data[h][N+1] = set_cntr;
+                                    set_cntr = set_counter(h, N, reordered_data_1);
+                                    reordered_data_1[h][N+1] = set_cntr;
                                 end
-                             else if(data[h][set_cntr] != 0 && hit_flag == 1)
+                             else if(reordered_data_1[h][set_cntr] != 0 && hit_flag == 1)
                                 begin
                                     internal_2 += 1;
-                                    set_cntr = set_counter(h, N, data);
-                                    data[h][N+1] = set_cntr;
+                                    set_cntr = set_counter(h, N, reordered_data_1);
+                                    reordered_data_1[h][N+1] = set_cntr;
                                 end
                              else
                                 begin
-                                    data[h][set_cntr] = entry[20:0];
+                                    reordered_data_1[h][set_cntr] = entry[20:0];
                                     internal_1 += 1;
-                                    set_cntr = set_counter(h, N, data);
-                                    data[h][N+1] = set_cntr;
+                                    set_cntr = set_counter(h, N, reordered_data_1);
+                                    reordered_data_1[h][N+1] = set_cntr;
                                 end   
                         end 
                 end
@@ -109,44 +150,44 @@ output reg [6:0] pass_2
     always @(trace2) begin
     for(b = 0; b < 58596; b = b + 1)
     begin
-    reordered_data [b] = data [b + 2];
-    reordered_data [b + 1] = data [b + 1];
-    reordered_data [b + 2] = data [b];
+    reordered_data_2 [b] = data_2 [b + 2];
+    reordered_data_2 [b + 1] = data_2 [b + 1];
+    reordered_data_2 [b + 2] = data_2 [b];
     end
     
         for(k = 0; k < 58596; k = i + 1)
             for(h = 0; h < K; h = h + 1)
                 begin
-                    if(data[h][0] == entry[21:20])
+                    if(reordered_data_2[h][0] == entry[21:20])
                         begin
                             k_inner = 1;
                             hit_flag = 0;
                             for(j = 0; j < N+1; j = j + 1)
                                 begin
-                                    if(data[h][k] == entry[20:0])
+                                    if(reordered_data_2[h][k] == entry[20:0])
                                         begin
                                             hit_flag = 1;
                                         end
                                 end
-                            if(data[h][set_cntr] == 0)
+                            if(reordered_data_2[h][set_cntr] == 0)
                                 begin
-                                    data[h][set_cntr] = entry[20:0];
+                                    reordered_data_2[h][set_cntr] = entry[20:0];
                                     internal_1 += 1;
-                                    set_cntr = set_counter(h, N, data);
-                                    data[h][N+1] = set_cntr;
+                                    set_cntr = set_counter_2(h, N, reordered_data_2);
+                                    reordered_data_2[h][N+1] = set_cntr;
                                 end
-                             else if(data[h][set_cntr] != 0 && hit_flag == 1)
+                             else if(reordered_data_2[h][set_cntr] != 0 && hit_flag == 1)
                                 begin
                                     internal_2 += 1;
-                                    set_cntr = set_counter(h, N, data);
-                                    data[h][N+1] = set_cntr;
+                                    set_cntr = set_counter_2(h, N, reordered_data_2);
+                                    reordered_data_2[h][N+1] = set_cntr;
                                 end
                              else
                                 begin
-                                    data[h][set_cntr] = entry[20:0];
+                                    reordered_data_2[h][set_cntr] = entry[20:0];
                                     internal_1 += 1;
-                                    set_cntr = set_counter(h, N, data);
-                                    data[h][N+1] = set_cntr;
+                                    set_cntr = set_counter_2(h, N, reordered_data_2);
+                                    reordered_data_2[h][N+1] = set_cntr;
                                 end   
                         end 
                 end
