7d1b5dcebcbdc200fa91ae56e730acb6a63c1f44
unknown <17803658+Brian-Dawson@users.noreply.github.com>
2021-05-03 10:39:14 -0400

    for jordan

--- lab4.srcs/sim_1/new/testbench.sv
+++ lab4.srcs/sim_1/new/testbench.sv
@@ -15,8 +15,12 @@ module testbench(
     reg trace2;
     wire [31:0] pass_1;
     wire [31:0] pass_2;
+    wire [31:0] pass_3;
+    wire [31:0] pass_4;
     reg [31:0] HIT;
     reg [31:0] MISS;
+    reg [31:0] HIT_2;
+    reg [31:0] MISS_2;
     reg [23:0] array_inputs1 [57961:0];
     reg [23:0] array_inputs2 [58596:0];
     
@@ -31,7 +35,9 @@ module testbench(
     .trace1(trace1),
     .trace2(trace2),
     .pass_1(pass_1),
-    .pass_2(pass_2)
+    .pass_2(pass_2),
+    .pass_3(pass_3),
+    .pass_4(pass_4)
     );
     
     initial begin
@@ -41,31 +47,27 @@ module testbench(
         K = 2;
         N = 128;
         trace1 = 0;
+        trace2 = 0;
     end
     
-    always @(*)
+    always @(posedge clk)
         begin
             MISS = pass_1;
             HIT = pass_2;
+            MISS_2 = pass_3;
+            HIT_2 = pass_4;
     end
     
-
     
    integer file_inputs2;
-   //integer scan_inputs2_num;
    integer file_inputs1;
-  // integer scan_inputs1_num;
-   
+
+
    reg [23:0] captured_inputs1;
    reg [23:0] captured_inputs2;
-  // reg [23:0] captured_inputs2 [0:59999];
-  // reg [23:0] captured_inputs1 [59999:0];
    
    initial begin
-   
-       // file_inputs1 = $fopen("D:/Verilogprojects/ELC463/lab4.srcs/sim_1/new/TRACE1.TXT", "r");
             file_inputs1 = $fopen("D:/Verilogprojects/ELC463/lab4.srcs/sim_1/new/TRACE1.DAT", "rb");
-        
         if(file_inputs1 == 0)
             begin
                 $display("file not found");
@@ -74,8 +76,6 @@ module testbench(
 end
 
     initial begin
-            
-        //file_inputs2 = $fopen("D:/Verilogprojects/ELC463/lab4.srcs/sim_1/new/trace2.txt", "r");
         file_inputs2 = $fopen("D:/Verilogprojects/ELC463/lab4.srcs/sim_1/new/TRACE2.DAT", "rb");
         
         if(file_inputs2 == 0)
@@ -87,18 +87,16 @@ end
    
    integer r;
    integer y;
+   
     initial begin
         r = 0;
         while(!$feof(file_inputs1))
             begin
                 captured_inputs1 = ($fgetc(file_inputs1)) | ($fgetc(file_inputs1) << 8) | ($fgetc(file_inputs1) << 16);
-                //scan_inputs1_num =  $fgets(captured_inputs1, file_inputs1);
-                //$display("captured input: %d file_input: %d", captured_inputs1, file_inputs1);
+
                 array_inputs1[r] = captured_inputs1;
                 r = r + 1;
-                //$display("Input: 0x%0h", captured_inputs1);
             end  
-            //$display("Number of references: 0x%0h", r);
                 $fclose(file_inputs1);
     end
     
@@ -107,13 +105,9 @@ end
         while(!$feof(file_inputs2))
             begin
                  captured_inputs2 = ($fgetc(file_inputs2)) | ($fgetc(file_inputs2) << 8) | ($fgetc(file_inputs2) << 16);
-                 //scan_inputs2_num = $fgets(captured_inputs2, file_inputs2);
-                 //$display("captured input: %d file_input: %d", captured_inputs2, file_inputs2);
                  array_inputs2[y] = captured_inputs2;
                  y = y + 1;
-                 //$display("0x%0h", captured_inputs2);
             end
-            //$display("0x%0h", y);
                 $fclose(file_inputs2);
     end
     
--- lab4.srcs/sources_1/new/cache_sim.v
+++ lab4.srcs/sources_1/new/cache_sim.v
@@ -4,47 +4,6 @@
 //K = 2
 //N = 128
 
-/*
-function set_counter(
-input index_loc, //H
-input [7:0] N,  //N always = 128
-input [23:0] cache [57961:0] //Whole cache
-);
-
-integer inter_set_cntr;
-inter_set_cntr = cache[index_loc][N+1];
-
-    if(inter_set_cntr == N)
-        begin
-            inter_set_cntr = 1;
-        end
-    else
-        begin
-            inter_set_cntr += 1;
-        end
-
-endfunction
-*/
-
-function set_counter_2(
-input index_loc,
-input [7:0] N,
-input [23:0] cache [58596:0]
-);
-
-integer inter_set_cntr;
-inter_set_cntr = cache[index_loc][N+1];
-
-    if(inter_set_cntr == N)
-        begin
-            inter_set_cntr = 1;
-        end
-    else
-        begin
-            inter_set_cntr += 1;
-        end
-
-endfunction
 
 module cache_main(
 input clk,
@@ -57,16 +16,16 @@ input [23:0] file2 [58596:0],
 input trace1,
 input trace2,
 output reg [31:0] pass_1,
-output reg [31:0] pass_2
+output reg [31:0] pass_2,
+output reg [31:0] pass_3, 
+output reg [31:0] pass_4
     );
     reg [23:0] data_1 [57961:0];
     reg [23:0] data_2 [58596:0];
     reg [23:0] cache[128:0][1:0];
-    
-    reg temp_data;
-    reg [7:0] data1_temp;
+    reg [23:0] cache_2[128:0][1:0];
     integer i = 0;
-    integer k = 0;
+    integer k = 0; 
     integer g = 0;
     integer h = 0;
     integer j = 0;
@@ -74,7 +33,11 @@ output reg [31:0] pass_2
     integer b = 0;
     integer k_inner = 0;
     reg [7:0] set_cntr = 0;
+    reg [7:0] set_cntr2 = 0;
     integer hit_flag = 0;
+    integer hit_flag_2 = 0;
+    integer temp = 0;
+    integer temp_2 = 0;
     
     integer top_cntr = 0;
     integer test_cntr1 = 0;
@@ -83,10 +46,14 @@ output reg [31:0] pass_2
     reg test_reg = 0;
     
     integer inter_set_cntr;
-    reg [31:0] internal_1 = 0; //MISS
-    reg [31:0] internal_2 = 0; //HIT
+    reg [31:0] internal_1 = 0; //MISS TRACE 1
+    reg [31:0] internal_2 = 0; //HIT TRACE 1
+    reg [31:0] internal_3 = 0;
+    reg [31:0] internal_4 = 0;
     
-    always@(*)begin
+
+
+    always @(posedge reset) begin
     for(int q = 0; q < (57962); q = q + 1)
     begin
     data_1[q] = file1[q];
@@ -95,37 +62,25 @@ output reg [31:0] pass_2
     begin
     data_2[o] = file2[o];
     end
-    end
- 
-
-    always @(reset) begin
-//    for(a = 0; a < 57962; a = a + 1)
-//    begin
-   
-//    data1_temp = data_1[a][23:16];
-//    data_1[a][23:16] = data_1[a][7:0];
-//    data_1[a][7:0] = data1_temp;
-    
-    
-//    end
-
     for(i = 0; i < 129; i = i + 1) //pre-populating cache with 0s
     begin
         for(h = 0; h < 2; h = h + 1)
         begin
             cache[i][h] = 0;
+            cache_2[i][h] = 0;
         end
     
     end
     
-    for(i = 0; i < 28981; i = i + 1)
+    for(int i = 0; i < 57961; i = i + 1)
         begin
-            for(h = 0; h < 2; h = h + 1)
+        //top_cntr +=1;
+            for(int h = 0; h < 2; h = h + 1)
                 begin
-                top_cntr +=1;
-                    if(cache[0][h][20] == data_1[i][20]) //if idx of trace data == index of cache
+                //top_cntr +=1;  
+                    if((cache[0][h][3] == data_1[i][3]) && temp < 57961) //if idx of trace data == index of cache
                         begin
-                            
+                            top_cntr +=1;
                             //k_inner = 1;
                             hit_flag = 0;
                             for(j = 0; j < N; j = j + 1)//setting hit flag
@@ -184,51 +139,91 @@ output reg [31:0] pass_2
                                             end
                                     cache[N][h] = set_cntr;
                                 end   
+                                temp = temp + 1;
+                        end 
+                end
+        end
+        
+         for(int i = 0; i < 58597; i = i + 1)
+        begin
+        //top_cntr +=1;
+            for(int h = 0; h < 2; h = h + 1)
+                begin
+                //top_cntr +=1;  
+                    if((cache_2[0][h][3] == data_2[i][3]) && temp_2 < 58597) //if idx of trace data == index of cache
+                        begin
+                            top_cntr +=1;
+                            //k_inner = 1;
+                            hit_flag_2 = 0;
+                            for(j = 0; j < N; j = j + 1)//setting hit flag
+                                begin
+                                    if(cache_2[j][h][20:0] == data_2[i][20:0])
+                                        begin
+                                            hit_flag_2 = 1;
+                                        end
+                                end
+                            if(cache_2[set_cntr2][h] == 0) //if space is empty
+                                begin
+                                    test_cntr1 += 1;
+                                    cache_2[set_cntr2][h][20:0] = data_2[i][20:0]; //fill empty space with tag
+                                    internal_3 += 1; //miss 
+                                    //set_cntr = set_counter(h, N, (data_1)); 
+                                    set_cntr2 = cache_2[N][h]; 
+                                        if(set_cntr2 == N-1)
+                                            begin
+                                                set_cntr2 = 0;
+                                            end
+                                        else
+                                            begin
+                                                set_cntr2 += 1;
+                                            end
+                                    
+                                    cache_2[N][h] = set_cntr2;
+                                end
+                             else if((cache_2[set_cntr2][h] != 0) && (hit_flag_2 == 1))//not empty and tag is same
+                                begin
+                                    test_cntr2 += 1;
+                                    internal_4 += 1; //hit
+                                    set_cntr2 = cache_2[N][h];
+                                        if(set_cntr2 == N-1)
+                                            begin
+                                                set_cntr2 = 0;
+                                            end
+                                        else
+                                            begin
+                                                set_cntr2 += 1;
+                                            end
+                                    cache_2[N][h] = set_cntr2;
+                                end
+                             else //not empty and not same tag
+                                begin
+                                    test_cntr3 += 1;
+                                    cache_2[set_cntr2][h][20:0] = data_2[i][20:0];
+                                    internal_3 += 1; //miss
+                                    set_cntr2 = cache_2[N][h];
+                                        if(set_cntr == N-1)
+                                            begin
+                                                set_cntr2 = 0;
+                                            end
+                                        else
+                                            begin
+                                                set_cntr2 += 1;
+                                            end
+                                    cache[N][h] = set_cntr2;
+                                end   
+                                temp_2 = temp_2 + 1;
                         end 
                 end
         end
     end
-  /*  
-    always @(*) begin
-        for(k = 0; k < 58597; k = i + 1)
-            for(h = 0; h < K; h = h + 1)
-                begin
-                    if(reordered_data_2[h][0] == entry[21:20])
-                        begin
-                            k_inner = 1;
-                            hit_flag = 0;
-                            for(j = 0; j < N+1; j = j + 1)
-                                begin
-                                    if(reordered_data_2[h][k] == entry[20:0])
-                                        begin
-                                            hit_flag = 1;
-                                        end
-                                end
-                            if(reordered_data_2[h][set_cntr] == 0)
-                                begin
-                                    reordered_data_2[h][set_cntr] = entry[20:0];
-                                    internal_1 += 1;
-                                    set_cntr = set_counter_2(h, N, reordered_data_2);
-                                    reordered_data_2[h][N+1] = set_cntr;
-                                end
-                             else if(reordered_data_2[h][set_cntr] != 0 && hit_flag == 1)
-                                begin
-                                    internal_2 += 1;
-                                    set_cntr = set_counter_2(h, N, reordered_data_2);
-                                    reordered_data_2[h][N+1] = set_cntr;
-                                end
-                             else
-                                begin
-                                    reordered_data_2[h][set_cntr] = entry[20:0];
-                                    internal_1 += 1;
-                                    set_cntr = set_counter_2(h, N, reordered_data_2);
-                                    reordered_data_2[h][N+1] = set_cntr;
-                                end   
-                        end 
-                end
-     end*/
+    
+    
+    
+    
      always @(posedge clk)begin
     pass_1 = internal_1;
     pass_2 = internal_2;
+    pass_3 = internal_3;
+    pass_4 = internal_4;
     end
 endmodule
